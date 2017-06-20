aa2e83ecb94326b674512108750279b4d07476ce
yasmeinesaker <yasmine_sq@hotmail.com>
2017-06-20 13:40:33 +0200

    Add files via upload

---
+++ Mensch.bot.py
@@ -0,0 +1,28 @@
+print("Hello can you interoduce yourself\n--- ")
+s=input()
+if "hello" in s:
+    print("Well, How can I help you")
+f=input()
+if "questions" in f:
+    print("Go on")
+a=input()
+if "hotel" in a:
+    print("How much can you pay for the rservation?")
+d=input()
+if "50" in d:
+    print("... that are some links that can help you") 
+w=input()
+if "restaurant" in w:
+    print("Which kind of food do you prefer?")
+t=input()
+if "Pizza hot" in t:
+    print("There is one which is close to the train station")
+z=input()
+if "location" in z:
+    print("Alexanderplatz")
+x=input()
+if "thank you" in x:
+    print("If you need more help, tell me")
+r=input()
+if "bye" in r:
+    print("Goodbye")
